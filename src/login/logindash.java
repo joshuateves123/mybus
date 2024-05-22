68a5ef27b3b33db624beae246ce122d5087e2cc6
joshuateves123 <joshuateves921@gmail.com>
2024-05-22 22:46:52 +0800

    MyBus App

---
+++ src/login/logindash.java
@@ -0,0 +1,296 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login;
+
+import admin.adminDash;
+import config.Session;
+import config.dbConnector;
+import config.passwordHasher;
+import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import staff.staff_book;
+import staff.staff_booking;
+
+/**
+ *
+ * @author w10
+ */
+public class logindash extends javax.swing.JFrame {
+
+    public logindash() {
+        initComponents();
+    }
+    
+    static String status;
+    static String type;
+    
+public static boolean loginAcc(String username, String password){
+        dbConnector connector = new dbConnector();
+        try{
+            String query = "SELECT * FROM tbl_users  WHERE u_username = '" + username+ "'";
+            ResultSet resultSet = connector.getData(query);
+            if(resultSet.next()){
+              
+                    String rehashedPass = passwordHasher.hashPassword(password);
+                    String hashedPass = resultSet.getString("u_password");
+                    if(hashedPass.equals(rehashedPass)){
+                        status = resultSet.getString("u_status");
+                        type = resultSet.getString("u_type");
+                        setSession(resultSet);
+                        return true;
+                    }else{
+                        return false;
+                    }
+            }else{
+                return false;
+            }
+                
+        }catch (SQLException | NoSuchAlgorithmException ex) {
+            return false;
+        }
+}
+
+public static void setSession(ResultSet resultSet) throws SQLException{
+        Session sess = Session.getInstance();
+        sess.setId(resultSet.getString("u_id"));
+        sess.setFname(resultSet.getString("u_fname"));
+        sess.setLname(resultSet.getString("u_lname"));
+        sess.setUsername(resultSet.getString("u_username"));
+        sess.setEmail(resultSet.getString("u_email"));
+        sess.setNum(resultSet.getString("u_num"));
+        sess.setPassword(resultSet.getString("u_password"));
+        sess.setType(resultSet.getString("u_type"));
+        sess.setStatus(resultSet.getString("u_status"));
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel5 = new javax.swing.JLabel();
+        jPanel7 = new javax.swing.JPanel();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jPanel5 = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        user = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jSeparator2 = new javax.swing.JSeparator();
+        jLabel6 = new javax.swing.JLabel();
+        pass = new javax.swing.JPasswordField();
+        jLabel8 = new javax.swing.JLabel();
+
+        jLabel5.setText("jLabel5");
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(153, 255, 153));
+
+        jPanel7.setBackground(new java.awt.Color(27, 66, 66));
+        jPanel7.setForeground(new java.awt.Color(255, 255, 255));
+        jPanel7.setLayout(null);
+
+        jPanel1.setBackground(new java.awt.Color(19, 93, 102));
+        jPanel1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(92, 131, 116), 10, true));
+        jPanel1.setForeground(new java.awt.Color(92, 131, 116));
+        jPanel1.setLayout(null);
+
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(158, 200, 185));
+        jLabel1.setText("Password");
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(20, 70, 90, 30);
+
+        jLabel2.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(158, 200, 185));
+        jLabel2.setText("Username");
+        jPanel1.add(jLabel2);
+        jLabel2.setBounds(20, 20, 100, 15);
+
+        jPanel2.setBackground(new java.awt.Color(0, 153, 153));
+        jPanel2.setLayout(null);
+
+        jPanel5.setBackground(new java.awt.Color(0, 153, 102));
+        jPanel5.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        jPanel2.add(jPanel5);
+        jPanel5.setBounds(-10, -10, 540, 270);
+
+        jLabel3.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel3.setText("Password:");
+        jPanel2.add(jLabel3);
+        jLabel3.setBounds(110, 430, 90, 17);
+
+        jLabel4.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel4.setText("Username:");
+        jPanel2.add(jLabel4);
+        jLabel4.setBounds(110, 380, 100, 17);
+
+        jTextField2.setText("jTextField1");
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jTextField2);
+        jTextField2.setBounds(200, 380, 190, 20);
+
+        jPanel1.add(jPanel2);
+        jPanel2.setBounds(0, 0, 0, 0);
+
+        user.setBackground(new java.awt.Color(238, 238, 238));
+        user.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                userActionPerformed(evt);
+            }
+        });
+        jPanel1.add(user);
+        user.setBounds(20, 40, 380, 30);
+
+        jButton1.setBackground(new java.awt.Color(118, 171, 174));
+        jButton1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jButton1.setText("Login");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton1);
+        jButton1.setBounds(20, 140, 380, 30);
+        jPanel1.add(jSeparator2);
+        jSeparator2.setBounds(20, 180, 380, 10);
+
+        jLabel6.setFont(new java.awt.Font("Calibri", 0, 14)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(158, 200, 185));
+        jLabel6.setText("Click here to register..");
+        jLabel6.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel6MouseClicked(evt);
+            }
+        });
+        jPanel1.add(jLabel6);
+        jLabel6.setBounds(150, 190, 140, 20);
+        jPanel1.add(pass);
+        pass.setBounds(20, 100, 380, 30);
+
+        jPanel7.add(jPanel1);
+        jPanel1.setBounds(470, 160, 420, 240);
+
+        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/media/BUSWAYZ.jpg"))); // NOI18N
+        jLabel8.setText("jLabel8");
+        jPanel7.add(jLabel8);
+        jLabel8.setBounds(0, -20, 930, 620);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel7, javax.swing.GroupLayout.PREFERRED_SIZE, 920, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel7, javax.swing.GroupLayout.PREFERRED_SIZE, 580, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jLabel6MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel6MouseClicked
+        registerdash rgst = new registerdash();
+        rgst.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_jLabel6MouseClicked
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        if(loginAcc(user.getText(),pass.getText())){
+            if(!status.equals("Active")){
+                JOptionPane.showMessageDialog(null,"Account is still pending to be approved by administrator!");
+            }else{
+                if(type.equals("Admin")){
+                    JOptionPane.showMessageDialog(null,"Login Success!");
+                    adminDash admin = new adminDash();
+                    admin.setVisible(true);
+                    this.dispose();
+                }else{
+                    JOptionPane.showMessageDialog(null,"Login Success!");
+                    staff_book staff = new staff_book();
+                    staff.setVisible(true);
+                    this.dispose();
+                }
+            }
+        }else{
+            JOptionPane.showMessageDialog(null,"Login Failed!");
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void userActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_userActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_userActionPerformed
+
+    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField2ActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(logindash.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(logindash.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(logindash.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(logindash.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new logindash().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel5;
+    private javax.swing.JPanel jPanel7;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JPasswordField pass;
+    private javax.swing.JTextField user;
+    // End of variables declaration//GEN-END:variables
+}
